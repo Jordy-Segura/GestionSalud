cf276b6923c0ab6058073ad98470172bf86ae635
Jordy-Segura <marcelo2005jmsp@gmail.com>
2025-05-12 16:08:32 -0500

    paciente

--- GestionCentroSalud/src/main/java/ec/edu/espoch/gestioncentrosalud/GestionCentroSalud.java
+++ GestionCentroSalud/src/main/java/ec/edu/espoch/gestioncentrosalud/GestionCentroSalud.java
@@ -11,6 +11,6 @@ package ec.edu.espoch.gestioncentrosalud;
 public class GestionCentroSalud {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
     }
 }
